fa230bd884303ec0cdc47e19b1ad8bc664163231
Pahuja <vanpahuja@deloitte.com>
2025-01-14 09:32:38 +0530

    minor

--- src/DropdownForm.js
+++ src/DropdownForm.js
@@ -39,91 +39,24 @@ const DropdownForm = () => {
     return selectedInputTypes.length === 0 || showForm;
   };
 
-  const handleToggle = () => {
-    if (!isDropDownOpen) {
-      setShowForm(false);
-    }
-    setIsDropDownOpen(!isDropDownOpen);
-  };
-
-  const btnstate = () => {
-    return selectedInputTypes.length === 0 || showForm;
-  };
-
-  const changeSelected = (value) => {
-    if (selectedInputTypes.includes(value)) {
-      setSelectedInputTypes(
-        selectedInputTypes.filter((item) => item !== value)
-      );
-    } else {
-      setSelectedInputTypes([...selectedInputTypes, value]);
-    }
-  };
-
-  const handleSub = () => {
-    setIsDropDownOpen(false);
-    setShowForm(true);
-  };
-
-  // return (
-  //   <div>
-  //     <button onClick={toggleDropdown}>
-  //       {isDropDownOpen ? 'Close Dropdown' : 'Open Dropdown'}
-  //     </button>
-
-  //     {isDropDownOpen && (
-  //       <div className="dropdown">
-  //         {inputTypes.map((inputType) => (
-  //           <div key={inputType.value}>
-  //             <input
-  //               type="checkbox"
-  //               id={inputType.value}
-  //               value={inputType.value}
-  //               checked={selectedInputTypes.includes(inputType.value)}
-  //               onChange={() => handleOptionChange(inputType.value)}
-  //             />
-  //             <label htmlFor={inputType.value}>{inputType.label}</label>
-  //           </div>
-  //         ))}
-  //         <button onClick={handleSubmit} disabled={handleBtnState()}>
-  //           Submit
-  //         </button>
-  //       </div>
-  //     )}
-
-  //     {showForm && (
-  //       <form>
-  //         {selectedInputTypes.map((inputType) => (
-  //           <div key={inputType}>
-  //             <label>{inputType}</label>
-  //             <input type={inputType} />
-  //           </div>
-  //         ))}
-  //       </form>
-  //     )}
-  //   </div>
-  // );
-
   return (
     <div>
       <button onClick={toggleDropdown}>
-        {isDropDownOpen ? "close" : "open"}
+        {isDropDownOpen ? 'Close Dropdown' : 'Open Dropdown'}
       </button>
 
       {isDropDownOpen && (
-        <div class="dropdown">
-          {inputTypes.map((item) => (
-            <div key={item.value}>
+        <div className="dropdown">
+          {inputTypes.map((inputType) => (
+            <div key={inputType.value}>
               <input
                 type="checkbox"
-                id={item.value}
-                checked={selectedInputTypes.includes(item.value)}
-                value={item.value}
-                onChange={() => {
-                  handleOptionChange(item.value);
-                }}
+                id={inputType.value}
+                value={inputType.value}
+                checked={selectedInputTypes.includes(inputType.value)}
+                onChange={() => handleOptionChange(inputType.value)}
               />
-              <label for={item.value}>{item.label}</label>
+              <label htmlFor={inputType.value}>{inputType.label}</label>
             </div>
           ))}
           <button onClick={handleSubmit} disabled={handleBtnState()}>
@@ -131,19 +64,20 @@ const DropdownForm = () => {
           </button>
         </div>
       )}
+
       {showForm && (
         <form>
-          {selectedInputTypes.map((item) => (
-            <div key={item}>
-              <label>{item}</label>
-              <input type={item}></input>
+          {selectedInputTypes.map((inputType) => (
+            <div key={inputType}>
+              <label>{inputType}</label>
+              <input type={inputType} />
             </div>
           ))}
         </form>
       )}
-      
     </div>
   );
+
 };
 
 export default DropdownForm;
